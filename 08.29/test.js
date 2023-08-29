879a22e6ef8f0d3cbb4a73899a3dc2e38316299b
Leejaeho1995 <p_n_s_m@naver.com>
2023-08-29 16:42:09 +0900

    ~08.29

---
+++ 08.29/test.js
@@ -0,0 +1,25 @@
+//동물 .. 이름 / 메소드 ->소리를 내다
+//개 ..동물 --> 이름 ,품종, 짖다
+
+class Animal{
+    constructor(name){
+        this.name= name
+    }
+    speak(){
+        console.log(`${this.name}가(이)소리를 내고있다.`)
+    }
+}
+
+class Dog extends Animal{
+    constructor(name,breed){
+        super(name)
+        this.breed =breed
+    }
+    speak(){
+        console.log(`${this.breed} 종류의 ${this.name}이(가) 멍멍하고 있다`)
+    }
+}
+const animal= new Animal('동물')
+animal.speak()
+const dog= new Dog('메리','시츄')
+dog.speak()
